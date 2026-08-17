5058bf05519a799ca4bd953dad15cc09037f988c
anshenellerutao71-max <anshenellerutao71@gmail.com>
2026-08-17 21:18:27 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function openLetter() {
+
+    document.getElementById("letter").style.display = "block";
+
+    document.getElementById("openButton").style.display = "none";
+
+}
+
+
+function answerYes() {
+
+    document.getElementById("answer").innerHTML =
+
+        "I'M SO HAPPY 😭❤️ Thank you for being honest with me!";
+
+}
+
+
+function answerNo() {
+
+    document.getElementById("answer").innerHTML =
+
+        "That's okay ❤️ Thank you for being honest. I still appreciate you.";
+
+}
